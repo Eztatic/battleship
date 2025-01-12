b5a0fa094568d7b6cab3d1a02beff9942a70f41a
Aaron <aakiswa2@gmail.com>
2025-01-12 22:00:54 +0800

    Initial setup

---
+++ dist/bundle.js
@@ -0,0 +1,8 @@
+/******/ (() => { // webpackBootstrap
+/*!*********************!*\
+  !*** ./src/main.js ***!
+  \*********************/
+// This is a comment
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7QUFBQSxvQiIsInNvdXJjZXMiOlsid2VicGFjazovL2JhdHRsZXNoaXAvLi9zcmMvbWFpbi5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyIvLyBUaGlzIGlzIGEgY29tbWVudFxuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
---
+++ src/ship.js
@@ -0,0 +1,7 @@
+class Ship {
+  constructor(length, hitCount, isSunk) {
+    this.length = length;
+    this.hitCount = hitCount;
+    this.isSunk = isSunk;
+  }
+}
