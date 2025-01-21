8aa7e0f2f49c008a0d288939c5ad3759555afebd
Aaron <aakiswa2@gmail.com>
2025-01-22 00:12:35 +0800

    Ongoing: creating UI for battleship

--- src/battleship.js
+++ src/battleship.js
@@ -1,9 +1,12 @@
+import './style.css';
+import './dom.js';
 import Gameboard from './gameboard.js';
 import Ship from './ship.js';
 import Player from './player.js';
 import Computer from './computer.js';
+import * as DOM from './dom.js';
 
-const player1 = new Player('Aaron', new Gameboard());
+const player1 = new Player('Player 1', new Gameboard());
 const computer = new Computer(new Gameboard());
 
 const ships = [];
@@ -15,16 +18,17 @@ function createShips(shipsLengths, ships) {
   });
 }
 
-function startGame(playerName) {
+function startGame() {
   // Create Ships
   createShips(shipsLengths, ships);
 
-  // Computer adds ships to board
+  // Computer adds ships to board randomly
   ships.forEach((ship) => {
     computer.placeShipRandomly(ship);
   });
 
-  // Get ships and present to player
+  // Render player board for ship placement
+  DOM.renderBoardCells(player1.gameboard.board);
 }
 
 export {computer, ships, createShips};
---
+++ src/dom.js
@@ -0,0 +1,19 @@
+const dialog = document.querySelector('dialog');
+const board1 = document.querySelector('.board1');
+
+// dialog.showModal();
+// dialog.classList.add('show');
+
+function renderBoardCells(playerBoard) {
+  playerBoard.forEach((row, x) => {
+    row.forEach((col, y) => {
+      const cell = document.createElement('div');
+      cell.classList.add('cell');
+      cell.dataset.x = x;
+      cell.dataset.y = y;
+      board1.appendChild(cell);
+    });
+  });
+}
+
+export {renderBoardCells};
